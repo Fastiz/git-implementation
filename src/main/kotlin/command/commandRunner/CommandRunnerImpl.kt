f3b10258f1b28e328d093840def234fb6aac4adc
fastiz <fastiz@itba.edu.ar>
2022-07-16 18:04:01 +0200

    add test to command runner

--- src/main/kotlin/command/commandRunner/CommandRunnerImpl.kt
+++ src/main/kotlin/command/commandRunner/CommandRunnerImpl.kt
@@ -3,8 +3,10 @@ package command.commandRunner
 import command.commit.Commit
 import command.commit.CommitImpl
 
-class CommandRunnerImpl(val commit: Commit = CommitImpl()) : CommandRunner {
+class CommandRunnerImpl(private val commit: Commit = CommitImpl()) : CommandRunner {
     override fun runCommit(args: Array<String>){
+        val parameters = args.toList().drop(1)
 
+        commit.run(parameters)
     }
 }
---
+++ src/test/kotlin/command/commandRunner/CommandRunnerImpl.kt
@@ -0,0 +1,32 @@
+package command.commandRunner
+
+import command.commit.Commit
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.runs
+import io.mockk.verify
+import org.junit.Before
+import org.junit.Test
+
+class CommandRunnerImplTestTest {
+    lateinit var commit: Commit
+    lateinit var commandRunnerImpl: CommandRunnerImpl
+
+    @Before
+    fun before(){
+        commit = mockk()
+        commandRunnerImpl = CommandRunnerImpl(commit = commit)
+    }
+
+    @Test
+    fun `commit calls commit command`(){
+        val args = listOf("commit", "a", "b").toTypedArray()
+
+        every { commit.run(any()) } just runs
+
+        commandRunnerImpl.runCommit(args)
+
+        verify { commit.run(listOf("a", "b")) }
+    }
+}
