88ad7ad9b4cfa3cf6dc73f4c9f0dddc2ff29f018
fastiz <fastiz@itba.edu.ar>
2022-07-26 13:33:48 +0200

    create commit test step

---
+++ src/test/kotlin/service/step/CreateCommitTest.kt
@@ -0,0 +1,43 @@
+package service.step
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Before
+import org.junit.Test
+import repository.commit.CommitRepository
+import service.commit.step.CreateCommit
+import service.commit.step.OutputCreateNewTree
+import kotlin.test.assertEquals
+
+class CreateCommitTest {
+    private lateinit var commitRepository: CommitRepository
+    private lateinit var createCommit: CreateCommit
+
+    @Before
+    fun before() {
+        commitRepository = mockk()
+        createCommit = CreateCommit(commitRepository)
+    }
+
+    @Test
+    fun `calls commit repository and returns correctly`() {
+        val input = OutputCreateNewTree(
+            treeId = "tree-id",
+            parentId = "parent-id"
+        )
+
+        every { commitRepository.create(any(), any(), any()) } returns "commit-id"
+
+        val result = createCommit.execute(input)
+
+        assertEquals("commit-id", result.commitId)
+        verify {
+            commitRepository.create(
+                treeId = "tree-id",
+                parentId = "parent-id",
+                message = ""
+            )
+        }
+    }
+}
