f6ab19245b1d8c08df53c1872b160609789e2cff
fastiz <fastiz@itba.edu.ar>
2022-07-26 15:40:05 +0200

    test CommitRepositoryImplTest.kt

---
+++ src/test/kotlin/repository/commit/CommitRepositoryImplTest.kt
@@ -0,0 +1,30 @@
+package repository.commit
+
+import dao.objects.objects.ObjectsDao
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class CommitRepositoryImplTest {
+    private lateinit var objectsDao: ObjectsDao
+    private lateinit var commitRepositoryImpl: CommitRepositoryImpl
+
+    @Before
+    fun before(){
+        objectsDao = mockk()
+        commitRepositoryImpl = CommitRepositoryImpl(objectsDao)
+    }
+
+    @Test
+    fun `create - calls objects dao`(){
+        every { objectsDao.createFromString(any()) } returns "commit-id"
+
+        val result = commitRepositoryImpl.create("tree-id", "parent-id", "")
+
+        verify { objectsDao.createFromString(any()) }
+        assertEquals("commit-id", result)
+    }
+}
