b62844c6396d2708561d80f4dce8ce11465a8d86
fastiz <fastiz@itba.edu.ar>
2022-09-25 19:58:28 +0200

    create refs and refs/heads directories

--- src/main/kotlin/service/init/InitServiceImpl.kt
+++ src/main/kotlin/service/init/InitServiceImpl.kt
@@ -4,16 +4,22 @@ import dao.files.FileDao
 import directory.Head
 import directory.Index
 import directory.Objects
+import directory.Refs
+import directory.RefsHeads
 
 class InitServiceImpl(
     private val objects: Objects,
     private val head: Head,
     private val index: Index,
+    private val refs: Refs,
+    private val refsHeads: RefsHeads,
     private val fileDao: FileDao,
 ) : InitService {
     override fun run() {
         fileDao.createDirectory(objects.path)
         fileDao.createFile(head.path)
         fileDao.createFile(index.path)
+        fileDao.createDirectory(refs.path)
+        fileDao.createDirectory(refsHeads.path)
     }
 }
